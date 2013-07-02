b4da0a0dbb323a9453c0aa433c1ce4d88e7308c0
Peter Suschlik <peter@suschlik.de>
2013-07-02 13:35:09 +0200

    Refactor Exposures tests a bit
    Before:
    23 tests, 40 assertions, 0 failures, 0 errors, 0 skips
    
    After:
    23 tests, 40 assertions, 0 failures, 0 errors, 0 skips

--- test/exposure/exposures_test.rb
+++ test/exposure/exposures_test.rb
@@ -2,16 +2,23 @@ require 'helper'
 
 require 'neo/rails/exposure/exposures'
 
-class ExposuresTest < NeoRailsCase
-  let(:exposures){ Neo::Rails::Exposure::Exposures.new([:a])}
+class ExposureExposuresTest < NeoRailsCase
+  let(:exposure_names) { [:a] }
+  let(:exposures) { Neo::Rails::Exposure::Exposures.new(exposure_names) }
 
-  test "exposed?" do
+  test "exposed? with value" do
+    refute exposures.exposed?(:a)
+    exposures[:a] = 1
+    assert exposures.exposed?(:a)
+  end
+
+  test "exposed? with nil" do
     refute exposures.exposed?(:a)
     exposures[:a] = nil
     assert exposures.exposed?(:a)
-    refute exposures.exposed?(:b)
-    exposures[:a] = 1
-    assert exposures.exposed?(:a)
+  end
+
+  test "exposed? for undeclared exposure" do
     refute exposures.exposed?(:b)
   end
 
@@ -23,7 +30,7 @@ class ExposuresTest < NeoRailsCase
 
   test "raises if a undefined key is set" do
     assert_raises Neo::Rails::Exposure::UndeclaredVariableError do
-      exposures[:c] = 1
+      exposures[:b] = 1
     end
   end
 end
